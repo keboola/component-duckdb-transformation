dfbe009a71a138e599d859bd9d280251606021f2
Josef Kudera <46950237+kudj@users.noreply.github.com>
2025-07-02 15:47:15 +0200

    added tests

---
+++ tests/test_functional.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+
+from datadirtest import DataDirTester
+from freezegun import freeze_time
+
+
+class TestComponent(unittest.TestCase):
+    @freeze_time("2023-04-02")
+    def test_functional(self):
+        os.environ["KBC_DATA_TYPE_SUPPORT"] = "none"
+        functional_tests = DataDirTester()
+        functional_tests.run()
+
+    @freeze_time("2023-04-02")
+    def test_functional_types(self):
+        os.environ["KBC_DATA_TYPE_SUPPORT"] = "authoritative"
+        functional_tests = DataDirTester(data_dir="./tests/functional_dtypes")
+        functional_tests.run()
+
+
+if __name__ == "__main__":
+    unittest.main()
